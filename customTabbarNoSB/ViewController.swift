8eff122c749f43e565e9fd6dd7ab22320cbbf625
sametkorkmaaz <smt.krkmz.2002@gmail.com>
2024-09-08 12:58:28 +0300

    Initial Commit

---
+++ customTabbarNoSB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  customTabbarNoSB
+//
+//  Created by Samet Korkmaz on 8.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
